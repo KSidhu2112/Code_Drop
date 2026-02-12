1569d2f1698008b42db2c27fa57e9925a1355235
kurvasidhu <kurvasidhu2112@gmail.com>
2026-02-12 23:16:43 +0530

    Update CORS origins to include localhost for local development

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['clinquant-kringle-dd3993.netlify.app', 'sweet-nougat-de4e24.netlify.app'], // Frontend and Admin URLs
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'clinquant-kringle-dd3993.netlify.app', 'sweet-nougat-de4e24.netlify.app'], // Frontend and Admin URLs
   credentials: true
 }));
 
